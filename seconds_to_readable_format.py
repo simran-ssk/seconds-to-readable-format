527164cb9fca69939b6677815b38a67b9b66f43e
Simran S. Khalsa <simransk@gmail.com>
2018-04-18 12:40:07 -0600

    Bug fix parse for days in time_to_convert.

--- seconds_to_readable_format.py
+++ seconds_to_readable_format.py
@@ -7,9 +7,10 @@ def seconds_to_readable_format(seconds):
     :return: String
     """
     time_to_convert = str(datetime.timedelta(seconds=seconds)).split(':')
-    if len(time_to_convert) > 3:
+    if 'days' in time_to_convert[0]:
+        time_to_convert = time_to_convert[0].split(', ') + time_to_convert[1:]
         return '{days} {hours} hours {minutes} minutes {seconds} seconds'.format(
-            days=time_to_convert[0], hours=time_to_convert[1], minutes=int(time_to_convert[2]), seconds=int(time_to_convert[3])
+            days=time_to_convert[0], hours=int(time_to_convert[1]), minutes=int(time_to_convert[2]), seconds=int(time_to_convert[3])
         )
     elif int(time_to_convert[0]) > 0:
         return '{hours} hours {minutes} minutes {seconds} seconds'.format(
